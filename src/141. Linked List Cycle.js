74d38804a111690809fe440c8cf4d93ee9fc1a24
Gavin Wang <gavinwang@192-168-1-8.tpgi.com.au>
2016-08-02 00:42:13 +1000

    141. Linked List Cycle.js:Add solution from a smart guy

--- src/141. Linked List Cycle.js
+++ src/141. Linked List Cycle.js
@@ -10,16 +10,31 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
+/**
+Suppose there is a cycle existing in the linked list, then runner will meet walker otherwise cycle does not exist.
+var hasCycle = function(head) {
+  if(head === null) return false
+  let walker = head
+  let runner = head
+  while(runner.next != null && runner.next.next != null){
+    walker = walker.next
+    runner = runner.next.next
+    if(walker.val === runner.val) return true
+  }
+  return false; 
+};
+ */
 // TODO: to be revised
 var hasCycle = function(head) {
-    if (head === null) return false;
-    var loop = function (node) {
-        if (node.visited) return true;
-        node.visited = true;
-        if (node.next === null) {
-            return false;
-        }
-        return loop(node.next);
-    };
-    return loop(head);
+  if (head === null) return false;
+  var loop = function(node) {
+    if (node.visited) return true;
+    node.visited = true;
+    if (node.next === null) {
+      return false;
+    }
+    return loop(node.next);
+  };
+  return loop(head);
 };
